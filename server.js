a475f5392cd844e3ed20f44cb224089e7e77d2cd
Chetan Luthra <chetanluthra9998@gmail.com>
2020-08-06 00:52:09 +0530

    Correcting the errors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const app = express();
 const path = require('path');
+const app = express();
 
 // Connect Database
 connectDB();
